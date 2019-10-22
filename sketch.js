9e0fb0ecf1ddb9ac6dc787e97bf49405106fab2c
Maria del Pilar Suarez Anzorena <pili.suarezan@gmail.com>
2019-10-22 23:34:21 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,43 @@
-function preload(){
-  // put preload code here
-}
+var capture;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  //VIDEO
+  capture = createCapture(VIDEO);
+  capture.size(640, 480);
+  capture.hide();
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+
+// VIDEO SIN FILTRO
+  var myFeed = capture.loadPixels();
+  image(myFeed, 0, 0, 640, 480);
+
+//FILTRO 1 CUANDO SE PRESIONA <-
+  if (keyIsDown(LEFT_ARROW)) {
+    image(myFeed, 0, 0, 0, 640, 480);
+    filter(THRESHOLD);
+  }
+
+//FILTRO 1 CUANDO SE PRESIONA ->
+  if (keyIsDown(RIGHT_ARROW)) {
+    image(myFeed, 0, 0, 0, 640, 480);
+    filter(INVERT);
+  }
+
+//FILTRO 1 CUANDO SE PRESIONA ^
+  if (keyIsDown(UP_ARROW)) {
+    image(myFeed, 0, 0, 0, 640, 480);
+    filter(GRAY);
+  }
+
+// TEXTO
+  textFont('Arial');
+  noStroke();
+  fill('255');
+  textSize(25);
+  text('- Choose your favourite filter by pressing the Key Arrows -', 0, 500);
 }
